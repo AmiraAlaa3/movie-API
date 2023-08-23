64d2e51945a1971e5f3485d715c867672c8c0f0b
Amira Alaa <meroana555666@gmail.com>
2023-08-23 23:37:49 +0300

    movie api

---
+++ main.js
@@ -0,0 +1,63 @@
+let category="now_playing";
+
+let imgPath = "https://image.tmdb.org/t/p/w500";
+let movieContainer=[];
+let linkName=document.getElementsByClassName("link-name");
+
+async function getData(category){
+    let response
+    if(category == "trendingLink")
+    {
+        response=await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=1a992e42ea2e2f4c31336ed1a99bf2ee`);
+    }
+    else{
+        response =await fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=1a992e42ea2e2f4c31336ed1a99bf2ee`);
+    }
+    let json=await response.json();
+    movieContainer=json.results;
+    console.log(movieContainer);
+    displayMovies();
+}
+
+getData(category);
+
+function displayMovies(){
+    let container=``;
+    for(let i=0;i<movieContainer.length;i++ ){
+        container+=`
+        <div class="col-sm-12 col-md-6 col-lg-4  mb-3">
+        <div class="content">
+             <div class="layer">
+                <h4>${movieContainer[i].title}</h4>
+                <p>overview: ${movieContainer[i].overview}</p>
+                <p>Vote: ${movieContainer[i].vote_average}</p>
+                <h6>${movieContainer[i].release_date}</h6>
+             </div>
+            <img src="${imgPath+movieContainer[i].poster_path}" alt="" class="w-100">
+        </div>
+        </div>`
+    }
+    document.getElementById('dataContainer').innerHTML=container;
+}
+function getCategory(e){
+    category = e.target.getAttribute('filmCategory');
+    getData(category);
+}
+Array.from(linkName).forEach(function(element){
+    element.addEventListener('click',getCategory);
+})
+let searchInput=document.getElementById("searchInput");
+async function search(){
+    let text=searchInput.value;
+    if(text == ''){
+        getData(category);
+    }
+    else{
+         let response= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=1a992e42ea2e2f4c31336ed1a99bf2ee&query=${text}`);
+    let json=await response.json();
+    movieContainer=json.results;
+    displayMovies();
+    }
+   
+}
+
